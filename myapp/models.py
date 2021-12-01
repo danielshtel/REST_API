17b90a84c98fa671d59c81b44a16e006ebd50fe6
daniel_shtel <97levchenko.danya@gmail.com>
2021-12-02 01:58:07 +0200

    add models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Driver(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'ID: {self.pk}. {self.first_name} {self.last_name}'
+
+
+class Vehicle(models.Model):
+    driver_id = models.ForeignKey('Driver', on_delete=models.PROTECT)
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    plate_number = models.CharField(max_length=15)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Vehicle ID:{self.pk}.\n' \
+               f'Driver ID:{self.driver_id}.\n' \
+               f'{self.make}, {self.model}, number: {self.plate_number}'
