19a92f7f88096f2a9dccca0cc122ff8274e13f3f
daniel_shtel <97levchenko.danya@gmail.com>
2021-12-06 19:08:02 +0200

    upd formatting datetime

--- myapp/converter.py
+++
@@ -1,3 +0,0 @@
-class DateConverter:
-   regex = r'[0-9]{2}-[0-9]{2}-[0-9]{4}'
-   format = '%d-%m-%Y'
--- myapp/serializer.py
+++ myapp/serializer.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 
 
 class DriverSerializer(serializers.HyperlinkedModelSerializer):
+    created_at = serializers.DateTimeField(format='%d/%m/%Y %H:%M:%S')
+    updated_at = serializers.DateTimeField(format='%d/%m/%Y %H:%M:%S')
     class Meta:
         model = Driver
         fields = ['first_name', 'last_name', 'created_at', 'updated_at']
