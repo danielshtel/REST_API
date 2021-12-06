77c629265ab0a0e33380a78f2b72980dd28679a7
daniel_shtel <97levchenko.danya@gmail.com>
2021-12-06 23:25:29 +0200

    upd

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,6 +4,6 @@ from .views import *
 ''
 
 urlpatterns = [
-    path('drivers/driver', drivers_list),
-    re_path(r'^drivers/driver/(?P<created_at__gte>)', DriverViewSet.as_view(), name='driver_filter')  # TODO url with parameters, driver list url
+    path('drivers/driver', DriverViewSet.as_view(), name='all_drivers'),
+    re_path(r'^drivers/driver/', DriverViewSet.as_view(), name='driver_filter'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,9 @@
 from datetime import datetime
 
-from django.core import serializers
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.http import JsonResponse, HttpResponseBadRequest
 from rest_framework.views import APIView
 
 from .models import Driver, Vehicle
-import json
-from django.http import JsonResponse, HttpResponse, HttpResponseNotFound, HttpResponseBadRequest
-from rest_framework import viewsets
 from .serializer import DriverSerializer
 
 
@@ -34,24 +29,7 @@ class DriverViewSet(APIView):
                 return JsonResponse(driver.data, safe=False)
             except ValueError:
                 return HttpResponseBadRequest('Bad request')
-
-
-def drivers_list(request):
-    drivers = Driver.objects.all()
-    serialized = json.loads(serializers.serialize('json', drivers, fields=('first_name', 'last_name',
-                                                                           'created_at', 'updated_at',)))
-    return JsonResponse(serialized, safe=False)
-
-###########################################################################
-# def driver_gte_date(request, *args, **kwargs):
-#     query = request.GET.get("created_at__gte", "")
-#     if query:
-#         try:
-#             driver = Driver.objects.filter(created_at__gte=datetime.strptime(query, '%d-%m-%Y'))
-#             serialized = json.loads(serializers.serialize('json', driver, fields=('first_name', 'last_name',
-#                                                                                   'created_at', 'updated_at',)))
-#
-#             return JsonResponse(serialized, safe=False)
-#         except ValueError:
-#             return HttpResponseBadRequest('Bad request')
-
+        else:
+            data = Driver.objects.all()
+            data = DriverSerializer(data, many=True)
+            return JsonResponse(data.data, safe=False)  # TODO DEFAULT GET METHOD AND OTHERS???????????
