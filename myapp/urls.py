fd1a59050f8b33626a9c4aff74ae1da2d38f047a
daniel_shtel <97levchenko.danya@gmail.com>
2021-12-08 18:40:42 +0200

    upd

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
 from django.urls import path, re_path
 from .views import *
-''
 
 urlpatterns = [
-    path('drivers/driver', DriverViewSet.as_view(), name='all_drivers'),
+    path('drivers/driver/<int:driver_id>/', DriverCRUDSet.as_view(), name='get_driver_pk'),
     re_path(r'^drivers/driver/', DriverViewSet.as_view(), name='driver_filter'),
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
+import json
 from datetime import datetime
-
+from dataclasses import dataclass
 from django.http import JsonResponse, HttpResponseBadRequest
 from rest_framework.views import APIView
 
@@ -8,7 +9,7 @@ from .serializer import DriverSerializer
 
 
 class DriverViewSet(APIView):
-    queryset = Driver.objects.all()
+    drivers_queryset = Driver.objects.all()
     serializer_class = DriverSerializer
 
     def get(self, request, *args, **kwargs):
@@ -16,20 +17,58 @@ class DriverViewSet(APIView):
         created_at_lte = self.request.query_params.get('created_at__lte')
         if created_at_gte is not None:
             try:
-                driver = Driver.objects.filter(created_at__gte=datetime.strptime(created_at_gte, '%d-%m-%Y'))
-                driver = DriverSerializer(driver, many=True)
-                return JsonResponse(driver.data, safe=False)
+                self.drivers_queryset = self.drivers_queryset.filter(
+                    created_at__gte=datetime.strptime(created_at_gte, '%d-%m-%Y')
+                )
             except ValueError:
                 return HttpResponseBadRequest('Bad request')
 
-        elif created_at_lte is not None:
+        if created_at_lte is not None:
             try:
-                driver = Driver.objects.filter(created_at__lte=datetime.strptime(created_at_lte, '%d-%m-%Y'))
-                driver = DriverSerializer(driver, many=True)
-                return JsonResponse(driver.data, safe=False)
+                self.drivers_queryset = self.drivers_queryset.filter(
+                    created_at__lte=datetime.strptime(created_at_lte, '%d-%m-%Y')
+                )
             except ValueError:
                 return HttpResponseBadRequest('Bad request')
-        else:
-            data = Driver.objects.all()
-            data = DriverSerializer(data, many=True)
-            return JsonResponse(data.data, safe=False)  # TODO DEFAULT GET METHOD AND OTHERS???????????
+
+        drivers = DriverSerializer(self.drivers_queryset, many=True)
+        return JsonResponse(drivers.data, safe=False)
+
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            new_driver = Driver(first_name=data['first_name'], last_name=data['last_name'])
+            new_driver.save()
+            serialized_driver = DriverSerializer(new_driver)
+            return JsonResponse(serialized_driver.data, safe=False)
+        except Exception as e:
+            return HttpResponseBadRequest(f'Exception: {e}')
+
+
+class DriverCRUDSet(APIView):
+    queryset = Driver.objects.all()
+    serializer_class = DriverSerializer
+
+    def get(self, request, *args, **kwargs):
+        try:
+            self.queryset = Driver.objects.get(pk=kwargs['driver_id'])
+            driver = DriverSerializer(self.queryset)
+            return JsonResponse(driver.data, safe=False)
+        except Exception as e:
+            return HttpResponseBadRequest(f'Exception: {e}')
+
+    def put(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        self.queryset = Driver.objects.get(pk=kwargs['driver_id'])
+        self.queryset.save()
+        upd_driver = DriverSerializer(self.queryset)
+        return JsonResponse(upd_driver.data) # TODO Validate json and keywords using dataclasses?
+
+
+# @dataclass
+# class DriverData:
+#     first_name: str = None
+#     last_name: str = None
+
+
+# driver_data = user_data = DriverData(**dict_from_json)
